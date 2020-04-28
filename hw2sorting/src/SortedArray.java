f97fa26a15e150dee5df12f1c08192df672caf0d
AhRaul <relian.skype@yandex.ru>
2020-04-28 23:49:39 +0300

    hw2 z 1 - 3 complete.

--- hw2sorting/src/SortedArray.java
+++ hw2sorting/src/SortedArray.java
@@ -1,3 +1,38 @@
 public class SortedArray {
 
+    private int size;
+    private int[] array;
+
+    public SortedArray(int size) {
+        this.size = 0;
+        this.array = new int[size];
+    }
+
+    public int[] getArray() {
+        return this.array;
+    }
+
+    public void display() {
+        for (int i = 0 ; i < this.size ; i ++) {
+            System.out.println(this.array[i]);
+        }
+    }
+
+    public void delete(int value) {
+        int i = 0;
+        for (i = 0; i < this.size; i++) {
+            if (this.array[i] == value) break;
+        }
+
+        for (int j = i; j < this.size - 1; j++) {
+            this.array[j] = this.array[j+1];
+        }
+
+        this.size--;
+    }
+
+    public void insert(int value) {
+        this.array[this.size] = value;
+        this.size++;
+    }
 }
--- hw2sorting/src/StartSorting.java
+++ hw2sorting/src/StartSorting.java
@@ -1,5 +1,9 @@
 public class StartSorting {
     public static void main(String[] args) {
-
+        SortedArray sa = new SortedArray(1000000);
+        for(int i = 0; i < 1000000; i++) {
+            sa.insert((int)(100 * Math.random()));
+        }
+        sa.display();
     }
 }
