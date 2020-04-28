0fdb836c77c8f5d34d3715e2b39403feab779c5b
AhRaul <relian.skype@yandex.ru>
2020-04-29 02:07:00 +0300

    hw2 z 4 complete.

--- hw2sorting/src/SortedArray.java
+++ hw2sorting/src/SortedArray.java
@@ -8,9 +8,6 @@ public class SortedArray {
         this.array = new int[size];
     }
 
-    public int[] getArray() {
-        return this.array;
-    }
 
     public void display() {
         for (int i = 0 ; i < this.size ; i ++) {
@@ -34,5 +31,56 @@ public class SortedArray {
     public void insert(int value) {
         this.array[this.size] = value;
         this.size++;
+    }
+
+    public void sortBubble() {
+        int out, in;
+        for (out = this.size - 1; out >=1; out--) {
+            for(in = 0; in < out; in++) {
+                if (this.array[in] > this.array[in + 1]) {
+                    change(in, in + 1);
+                }
+            }
+        }
+    }
+
+    private void change(int a, int b) {
+        int tmp = this.array[a];
+        this.array[a] = this.array[b];
+        this.array[b] = tmp;
+    }
+
+    public void sortSelect() {
+        int out, in, mark;
+        for(out = 0; out < this.size; out++) {
+            mark = out;
+            for(in = out+1; in<this.size; in++) {
+                if (this.array[in] < this.array[mark]) {
+                    mark = in;
+                }
+            }
+            change(out, mark);
+        }
+    }
+
+    public void sortInsert() {
+        int in, out;
+        for(out = 1; out < this.size; out++) {
+            int temp = this.array[out];
+            in = out;
+            while (in > 0 && this.array[in - 1] >= temp) {
+                this.array[in] = this.array[in - 1];
+                --in;
+            }
+            this.array[in] = temp;
+        }
+    }
+
+    public SortedArray clone() {
+        SortedArray sortedArray = new SortedArray(this.size);
+        for(int i = 0; i < this.size; i++) {
+            sortedArray.insert(this.array[i]);
+        }
+        return sortedArray;
     }
 }
--- hw2sorting/src/StartSorting.java
+++ hw2sorting/src/StartSorting.java
@@ -1,9 +1,30 @@
 public class StartSorting {
+    public static final int SIZE_ARRAY = 1000000;
+
     public static void main(String[] args) {
-        SortedArray sa = new SortedArray(1000000);
-        for(int i = 0; i < 1000000; i++) {
+        SortedArray sa = new SortedArray(SIZE_ARRAY);
+        for(int i = 0; i < SIZE_ARRAY; i++) {
             sa.insert((int)(100 * Math.random()));
         }
-        sa.display();
+
+        SortedArray sarepid1 = sa.clone();
+
+        SortedArray sarepid2 = sa.clone();
+
+        long startTime = System.currentTimeMillis();
+        sa.sortBubble();
+        long timeSpent = System.currentTimeMillis() - startTime;
+        System.out.println("Время пузырьковой сортировки " + timeSpent); //1756661
+
+
+        startTime = System.currentTimeMillis();
+        sarepid1.sortSelect();
+        timeSpent = System.currentTimeMillis() - startTime;
+        System.out.println("Время сортировки выборкой " + timeSpent);   //383415
+
+        startTime = System.currentTimeMillis();
+        sarepid2.sortInsert();
+        timeSpent = System.currentTimeMillis() - startTime;
+        System.out.println("Время сортировки вставкой " + timeSpent);   //100388
     }
 }
