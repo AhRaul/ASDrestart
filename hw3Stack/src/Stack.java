4a47951426b8f5ce467ca88a560deb21a2e1c78b
AhRaul <relian.skype@yandex.ru>
2020-05-04 16:02:51 +0300

    hw3 z 1 complete.

---
+++ hw3Stack/src/Stack.java
@@ -0,0 +1,32 @@
+public class Stack {
+
+    private int maxSize;
+    private int[] stack;
+    private int top;
+
+    public Stack(int maxSize) {
+        this.maxSize = maxSize;
+        this.stack = new int[this.maxSize];
+        this.top = -1;
+    }
+
+    public boolean isEmpty() {
+        return (this.top == -1);
+    }
+
+    public boolean isFull() {
+        return (this.top == this.maxSize - 1);
+    }
+
+    public void push(int i) {
+        this.stack[++this.top] = i;
+    }
+
+    public int peekAndDelete() {
+        return this.stack[this.top--];
+    }
+
+    public int peek() {
+        return this.stack[this.top];
+    }
+}
